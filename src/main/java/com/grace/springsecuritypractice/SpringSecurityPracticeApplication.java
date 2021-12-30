d09d4db797f98f0e117d0e4eaf86d549a304a5f2
berryberry189 <berryberry189@naver.com>
2021-12-30 20:42:22 +0900

    Initial commit

---
+++ src/main/java/com/grace/springsecuritypractice/SpringSecurityPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.grace.springsecuritypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+	}
+
+}
