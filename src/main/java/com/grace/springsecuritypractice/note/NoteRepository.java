712f32370d0974121ea116d8fc737e99bd40213f
berryberry189 <berryberry189@naver.com>
2021-12-31 21:12:26 +0900

    Feat Note 개인노트 기능

---
+++ src/main/java/com/grace/springsecuritypractice/note/NoteRepository.java
@@ -0,0 +1,14 @@
+package com.grace.springsecuritypractice.note;
+
+
+import com.grace.springsecuritypractice.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    List<Note> findByUserOrderByIdDesc(User user);
+
+    Note findByIdAndUser(Long id, User user);
+}
